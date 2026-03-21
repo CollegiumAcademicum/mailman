4d2e429361f770ba7723e10cb97a1654f6f863c0
Philipp <162928651+philipp-btz@users.noreply.github.com>
2026-03-22 00:43:47 +0100

    debloat

--- ping.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-try:
-    print("Attempting to connect...")
-    response = requests.get('https://chat.collegiumacademicum.de/api/v4/system/ping', timeout=5)
-    print(f"Success! Server replied: {response.text}")
-except Exception as e:
-    print(f"Connection Failed: {e}")
