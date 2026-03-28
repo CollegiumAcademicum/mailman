4ec0670ea6bbd7aa2e035dde8b4339801e3fa271
philipp-btz <162928651+philipp-btz@users.noreply.github.com>
2026-03-28 20:48:23 +0000

    style: auto-format python code with ruff

--- tests/success_message.py
+++ tests/success_message.py
@@ -7,8 +7,8 @@ BOT_TOKEN = sys.argv[2]
 TEAM_NAME = sys.argv[3]
 
 driver = Driver(
-        {"url": MATTERMOST_URL, "token": BOT_TOKEN, "scheme": "https", "port": 443}
-    )
+    {"url": MATTERMOST_URL, "token": BOT_TOKEN, "scheme": "https", "port": 443}
+)
 
 driver.login()
 bot_id = driver.users.get_user("me")["id"]
@@ -16,15 +16,14 @@ bot_username = driver.users.get_user("me")["username"]
 team_id = driver.teams.get_team_by_name(TEAM_NAME)["id"]
 
 
-
 print(f"Bot connected. Bot ID: {bot_id} | Team ID: {team_id}")
 
 driver.posts.create_post(
-            {
-                "channel_id": "14d9s71is3fh3duwj9a6u9k4jr",
-                "message": "unit test completed",
-            }
-        )
+    {
+        "channel_id": "14d9s71is3fh3duwj9a6u9k4jr",
+        "message": "unit test completed",
+    }
+)
 
 
 driver.logout()
